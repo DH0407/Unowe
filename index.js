a6812207b5ebf47244ce4928926525ff50390dee
hansukhyeon3 <hhans6245@gmail.com>
2024-03-07 17:38:00 +0900

    03-07

--- index.js
+++ index.js
@@ -15,3 +15,21 @@ document.addEventListener('DOMContentLoaded', function() {
         menuButton.classList.toggle('hiddenButtonani');
     });
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    var offButton = document.getElementById('offButton');
+    var rectangle = document.getElementById('rectangle');
+  
+    offButton.addEventListener('click', function() {
+        rectangle.classList.toggle('animateoff');
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    var offButton = document.getElementById('offButton');
+    var offButton = document.getElementById('offButton');
+  
+    offButton.addEventListener('click', function() {
+        menuButton.classList.toggle('hiddenButtonanioff');
+    });
+});
