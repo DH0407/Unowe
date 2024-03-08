7d58c6f7dd9d39dd256ca85478799dfa51bbd333
DH0407 <byeolb96@gmail.com>
2024-03-08 09:47:26 +0900

    됐나?

--- index.js
+++ index.js
@@ -1,82 +1,23 @@
-// document.addEventListener('DOMContentLoaded', function() {
-//     var menuButton = document.getElementById('menuButton');
-//     var rectangle = document.getElementById('rectangle');
-  
-//     menuButton.addEventListener('click', function() {
-//         rectangle.classList.toggle('animate');
-//         menuButton.classList.toggle('hiddenButtonani');
-//     });
-// });
-
-
 document.addEventListener('DOMContentLoaded', function() {
-    var menuButton = document.getElementById('menuButton');
-    var offButton = document.getElementById('offButton');
-    var rectangle = document.getElementById('rectangle');
+    var mainAreaMenuButton = document.getElementById('mainAreaMenuButton');
+    var menuAreaButton = document.getElementById('menuAreaButton');
+    var mainArea = document.getElementById('mainArea');
   
-    menuButton.addEventListener('click', function() {
+    mainAreaMenuButton.addEventListener('click', function() {
         // 메뉴 버튼을 클릭하여 메뉴를 열 때
-        rectangle.classList.add('animate');
-        rectangle.classList.remove('animateoff'); // animateoff 클래스 제거
+        mainArea.classList.add('mainAreaMoveRight');
+        mainArea.classList.remove('mainAreaMoveLeft'); // mainAreaMoveLeft 클래스 제거
         
         // 메뉴 버튼 서서히 사라지게 하기
-        menuButton.classList.add('hidden');
+        mainAreaMenuButton.classList.add('mainAreaMenuButtonHidden');
     });
-    
-    offButton.addEventListener('click', function() {
+   
+    menuAreaButton.addEventListener('click', function() {
         // off 버튼을 클릭하여 메뉴를 닫을 때
-        rectangle.classList.remove('animate'); // animate 클래스 제거
-        rectangle.classList.add('animateoff');
+        mainArea.classList.remove('mainAreaMoveRight'); // mainAreaMoveRight 클래스 제거
+        mainArea.classList.add('mainAreaMoveLeft');
         
         // 메뉴 버튼 서서히 나타나게 하기
-        menuButton.classList.remove('hidden');
+        mainAreaMenuButton.classList.remove('mainAreaMenuButtonHidden');
     });
-    
-    
 });
-
-// document.addEventListener('DOMContentLoaded', function() {
-//     var menuButton = document.getElementById('menuButton');
-//     var rectangle = document.getElementById('rectangle');
-  
-//     menuButton.addEventListener('click', function() {
-//         rectangle.classList.toggle('animate');
-//         menuButton.classList.toggle('hiddenButtonani');
-//     });
-// });
-
-// document.addEventListener('DOMContentLoaded', function() {
-//     var offButton = document.getElementById('offButton');
-//     var rectangle = document.getElementById('rectangle');
-//     var menuButton = document.getElementById('menuButton');
-  
-//     offButton.addEventListener('click', function() {
-//         rectangle.classList.add('animateoff');
-//         setTimeout(function() {
-//             menuButton.classList.toggle('hiddenButtonani');
-//         }, 1000); // 1초 후에 hiddenButtonani 클래스를 토글
-//     });
-// });
-    
-
-
-
-
-
-// document.addEventListener('DOMContentLoaded', function() {
-//     var menuButton = document.getElementById('menuButton');
-//     var menuButton = document.getElementById('menuButton');
-  
-//     menuButton.addEventListener('click', function() {
-//         menuButton.classList.toggle('hiddenButtonani');
-//     });
-// });
-
-// document.addEventListener('DOMContentLoaded', function() {
-//     var offButton = document.getElementById('offButton');
-//     var offButton = document.getElementById('offButton');
-  
-//     offButton.addEventListener('click', function() {
-//         menuButton.classList.toggle('hiddenButtonanioff');
-//     });
-// });
