c128e82bd7d2461bdde828f2f6b658b8c76811e8
hansukhyeon3 <hhans6245@gmail.com>
2024-03-10 21:49:28 +0900

    클라이언트 창

--- index.js
+++ index.js
@@ -11,30 +11,41 @@ document.addEventListener('DOMContentLoaded', function() {
         ClientInformationZoon.classList.add('ClientInformationMoveLeft');
         ClientInformationZoon.classList.remove('ClientInformationMoveRight');
         mainAreaMenuButton.classList.add('menuAreaMenuButtonHidden');
+        ClientInformationZoon.classList.remove('ClientInformationMoveUpRight');
+
     });
 
     menuAreaButton.addEventListener('click', function() {
+        if(readMoreButton.id == 'readMoreButton') {
+            ClientInformationZoon.classList.add('ClientInformationMoveRight');
+            ClientInformationZoon.classList.remove('ClientInformationMoveUpRight');
+        }else if(readMoreButton.id == 'readMoreOff') {
+            ClientInformationZoon.classList.add('ClientInformationMoveUpRight');
+            ClientInformationZoon.classList.remove('ClientInformationMoveDown');
+            readMoreButton.id = 'readMoreButton';
+        }
         mainArea.classList.remove('mainAreaMoveRight');
         mainArea.classList.add('mainAreaMoveLeft');
-        ClientInformationZoon.classList.add('ClientInformationMoveRight');
         ClientInformationZoon.classList.remove('ClientInformationMoveLeft');
         mainAreaMenuButton.classList.remove('menuAreaMenuButtonHidden');
+        
+            
     });
 
     readMoreButton.addEventListener('click', function() {
-        switch (readMoreButton.id) {
-            case 'readMoreButton':
-                readMoreButton.id = 'readMoreOff';
-                ClientInformationZoon.classList.add('ClientInformationMoveDown');
-                ClientInformationZoon.classList.remove('ClientInformationMoveUp');
-                console.log('On');
-                break;
-            case 'readMoreOff':
-                readMoreButton.id = 'readMoreButton';
-                ClientInformationZoon.classList.add('ClientInformationMoveUp');
-                ClientInformationZoon.classList.remove('ClientInformationMoveDown');
-                console.log('Off');
-                break;
-        }
+            switch (readMoreButton.id) {
+                case 'readMoreButton':
+                    readMoreButton.id = 'readMoreOff';
+                    ClientInformationZoon.classList.add('ClientInformationMoveDown');
+                    ClientInformationZoon.classList.remove('ClientInformationMoveUp');
+                    console.log('On');
+                    break;
+                case 'readMoreOff':
+                    readMoreButton.id = 'readMoreButton';
+                    ClientInformationZoon.classList.add('ClientInformationMoveUp');
+                    ClientInformationZoon.classList.remove('ClientInformationMoveDown');
+                    console.log('Off');
+                    break;
+            }
     });
 });
