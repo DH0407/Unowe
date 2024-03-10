58e250b3fc6b7abf3f1adcb84601b66ea3ce097c
DH0407 <byeolb96@gmail.com>
2024-03-10 21:13:38 +0900

    제발!!

--- index.js
+++ index.js
@@ -2,29 +2,41 @@ document.addEventListener('DOMContentLoaded', function() {
     var mainAreaMenuButton = document.querySelector('.mainAreaMenuButton');
     var menuAreaButton = document.getElementById('menuAreaButton');
     var mainArea = document.getElementById('mainArea');
-    var ClientInformationZoon = document.querySelector('.ClientInformationZoon'); // 변경: ID에서 클래스로 수정
-  
-    mainAreaMenuButton.addEventListener('click', function() {
-        // 메뉴 버튼을 클릭하여 메뉴를 열 때
-        mainArea.classList.add('mainAreaMoveRight');
-        mainArea.classList.remove('mainAreaMoveLeft'); // mainAreaMoveLeft 클래스 제거
-        ClientInformationZoon.classList.add('ClientInformationMoveLeft');
-        ClientInformationZoon.classList.remove('ClientInformationMoveRight');  
+    var ClientInformationZoon = document.querySelector('.ClientInformationZoon');
+    var readMoreButton = document.getElementById('readMoreButton')
 
-        // 메뉴 버튼 서서히 사라지게 하기
-        mainAreaMenuButton.classList.add('menuAreaMenuButtonHidden'); // 클래스 이름 변경
+    mainAreaMenuButton.addEventListener('click', function() {
+        mainArea.classList.add('mainAreaMoveRight');
+        mainArea.classList.remove('mainAreaMoveLeft');
+        ClientInformationZoon.classList.add('ClientInformationMoveLeft');
+        ClientInformationZoon.classList.remove('ClientInformationMoveRight');
+        mainAreaMenuButton.classList.add('menuAreaMenuButtonHidden');
     });
-   
+
     menuAreaButton.addEventListener('click', function() {
-        // off 버튼을 클릭하여 메뉴를 닫을 때
-        mainArea.classList.remove('mainAreaMoveRight'); // mainAreaMoveRight 클래스 제거
+        mainArea.classList.remove('mainAreaMoveRight');
         mainArea.classList.add('mainAreaMoveLeft');
-        ClientInformationZoon.classList.add('ClientInformationMoveRight'); 
+        ClientInformationZoon.classList.add('ClientInformationMoveRight');
         ClientInformationZoon.classList.remove('ClientInformationMoveLeft');
-        
-        // 메뉴 버튼 서서히 나타나게 하기
-        mainAreaMenuButton.classList.remove('menuAreaMenuButtonHidden'); // 클래스 이름 변경
+        mainAreaMenuButton.classList.remove('menuAreaMenuButtonHidden');
+    });
+
+    readMoreButton.addEventListener('click', function() {
+        switch (readMoreButton.id) {
+            case 'readMoreButton':
+                readMoreButton.id = 'readMoreOff';
+                ClientInformationZoon.classList.add('ClientInformationMoveDown');
+                ClientInformationZoon.classList.remove('ClientInformationMoveUp');
+                console.log('On');
+                console.log(readMoreButton.className);
+                break;
+            case 'readMoreOff':
+                readMoreButton.id = 'readMoreButton';
+                ClientInformationZoon.classList.add('ClientInformationMoveUp');
+                ClientInformationZoon.classList.remove('ClientInformationMoveDown');
+                console.log('Off');
+                console.log(readMoreButton.className);
+                break;
+        }
     });
 });
-
-
