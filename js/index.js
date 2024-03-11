1ee8d3fa96ecba8b6f4ab04d67ee5bf0c05f6900
hansukhyeon3 <hhans6245@gmail.com>
2024-03-12 01:11:14 +0900

    쿠키 설정

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+//애니메이션 js코드
 document.addEventListener('DOMContentLoaded', function() {
     var mainAreaMenuButton = document.querySelector('.mainAreaMenuButton');
     var menuAreaButton = document.getElementById('menuAreaButton');
@@ -23,3 +24,30 @@ document.addEventListener('DOMContentLoaded', function() {
         loginButton.classList.remove('loginZoonMoveLeft');
     });
 });
+//쿠키 관련 js코드
+function getCookies(name) {
+    var nameEQ = name + "=";
+    var cookies = document.cookie.split(';');
+
+    for (var i = 0; i < cookies.length; i++) {
+        var cookie = cookies[i];
+        while (cookie.charAt(0) == ' ') {
+            cookie = cookie.substring(1, cookie.length);
+        }
+        if (cookie.indexOf(nameEQ) == 0) {
+            return cookie.substring(nameEQ.length, cookie.length);
+        }
+    }
+    return null;
+}
+
+document.addEventListener("DOMContentLoaded", onPageLoad);
+
+function onPageLoad() {
+    var loginCheck = getCookies("isLoggedIn");
+
+    if (loginCheck == "true") {
+        location.href = '../indexLoginO.html';
+    }
+    
+}
