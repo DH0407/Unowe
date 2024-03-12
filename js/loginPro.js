6b50b0bc938014156bed11dfc99ff502eb0f7286
hansukhyeon3 <hhans6245@gmail.com>
2024-03-12 13:46:42 +0900

    쿠키 저장 30일 -> 1일

--- js/loginPro.js
+++ js/loginPro.js
@@ -1,9 +1,9 @@
 document.addEventListener("DOMContentLoaded", onPageLoad);
 function onPageLoad() {
     console.log("페이지가 로드되었습니다.");
-    // 쿠키 만료 날짜를 설정 (예: 현재 날짜로부터 30일 후)
+    // 쿠키 만료 날짜를 설정 (예: 현재 날짜로부터 1일 후)
     var expirationDate = new Date();
-    expirationDate.setDate(expirationDate.getDate() + 30);
+    expirationDate.setDate(expirationDate.getDate() + 1);
 
     // 쿠키 설정
     document.cookie = "isLoggedIn=true; expires=" + expirationDate.toUTCString() + "; path=/";
