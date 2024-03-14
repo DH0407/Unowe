3fbe4ea077ba58a3315c2a2be4f68492eb7be43d
hansukhyeon3 <hhans6245@gmail.com>
2024-03-14 20:13:57 +0900

    애니메이션

--- js/index.js
+++ js/index.js
@@ -3,7 +3,14 @@ document.addEventListener('DOMContentLoaded', function() {
     var menuAreaButton = document.getElementById('menuAreaButton');
     var mainArea = document.getElementById('mainArea');
     var loginZoon = document.querySelector('.loginZoon');
-    var loginButton = document.getElementById('loginButton')
+    var loginButton = document.getElementById('loginButton');
+    var img1 = document.getElementById('img1');
+    var img2 = document.getElementById('img2');
+    var img3 = document.getElementById('img3');
+    var lefttMoveButton = document.getElementById('lefttMoveButton');
+    var rightMoveButton = document.getElementById('rightMoveButton');
+
+    var moveAdvertisementCheck = 1;
 
     mainAreaMenuButton.addEventListener('click', function() {
         mainArea.classList.add('mainAreaMoveRight');
@@ -22,6 +29,53 @@ document.addEventListener('DOMContentLoaded', function() {
         loginZoon.classList.remove('ClientInformationMoveDown');
         loginButton.classList.remove('loginZoonMoveLeft');
     });
+
+    rightMoveButton.addEventListener('click', function() {
+        if(moveAdvertisementCheck == 1){
+            img1.classList.add('imgNumber1Move1');
+            img1.classList.remove('imgNumber1Move2');
+            img1.classList.remove('imgNumber1Move3');
+
+            img2.classList.remove('imgNumber2Move1');
+            img2.classList.add('imgNumber2Move2');
+            img2.classList.remove('imgNumber2Move3');
+
+            img3.classList.remove('imgNumber3Move1');
+            img3.classList.remove('imgNumber3Move2');
+            img3.classList.add('imgNumber3Move3');
+
+            moveAdvertisementCheck = 2
+        }else if(moveAdvertisementCheck == 2) {
+            img1.classList.remove('imgNumber1Move1');
+            img1.classList.add('imgNumber1Move2');
+            img1.classList.remove('imgNumber1Move3');
+
+            img2.classList.remove('imgNumber2Move1');
+            img2.classList.remove('imgNumber2Move2');
+            img2.classList.add('imgNumber2Move3');
+
+            img3.classList.add('imgNumber3Move1');
+            img3.classList.remove('imgNumber3Move2');
+            img3.classList.remove('imgNumber3Move3');
+
+            moveAdvertisementCheck = 3
+        }else if(moveAdvertisementCheck == 3) {
+            img1.classList.remove('imgNumber1Move1');
+            img1.classList.remove('imgNumber1Move2');
+            img1.classList.add('imgNumber1Move3');
+
+            img2.classList.add('imgNumber2Move1');
+            img2.classList.remove('imgNumber2Move2');
+            img2.classList.remove('imgNumber2Move3');
+
+            img3.classList.remove('imgNumber3Move1');
+            img3.classList.add('imgNumber3Move2');
+            img3.classList.remove('imgNumber3Move3');
+
+            moveAdvertisementCheck = 1
+        }
+    });
+
 });
 //쿠키 관련 js코드
 function getCookies(name) {
