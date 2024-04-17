b888e10d1ce3c1981152fffe00eeff0841e66350
Yongwon14 <jangyongwon72@gmail.com>
2024-04-17 18:08:45 +0900

    인덱스, 로그인

---
+++ js/login.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+  setTimeout(function() {
+      document.getElementById('unoweLongText').style.opacity = 0;
+    }, 30);
+
+  setTimeout(function() {
+    document.getElementById('loginInputArea').style.opacity = 1;
+  }, 20);
+});
